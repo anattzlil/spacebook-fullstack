bd76ea93f06c608d3b37b5cf1b0aab3590cc2873
anattzlil <anattzlil@gmail.com>
2018-01-22 10:41:51 +0200

    fixing server for heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/spacebookDB', function () {
+mongoose.connect(process.env.CONNECTION_STRING || 'mongodb://localhost/spacebookDB', function () {
   console.log("DB connection established!!!");
 })
 
